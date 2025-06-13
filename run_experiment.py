57073528f467b9297560f533ffd5afa8e268a053
YCC-DAVID <879198191@qq.com>
2025-06-13 23:52:57 +0800

    11

--- run_experiment.py
+++ run_experiment.py
@@ -7,15 +7,20 @@ def run_task(param):
     print(f"python /home/chence/workspace/shm_detection/freezing/smh_detection_code/main_model.py {param}")
 
 
-params = ["-epo 50 --src_path /home/shared_data/salmonella_detection/AugmentedData/AmericanDataAug -logger -comb_ds -p 4 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
-          "-epo 50 --src_path /home/shared_data/salmonella_detection/AugmentedData/AmericanDataAug -logger -comb_ds -p 3 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
-          "-epo 50 --src_path /home/shared_data/salmonella_detection/AugmentedData/AmericanDataAug -logger -comb_ds -p 2 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
-          "-epo 50 --src_path /home/shared_data/salmonella_detection/AugmentedData/AmericanDataAug -logger -comb_ds -p 1 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
-          "-epo 50 --src_path /home/shared_data/salmonella_detection/AugmentedData/AmericanDataAug -logger -comb_ds -p 0 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
-          "-epo 50 --src_path /home/shared_data/salmonella_detection/AugmentedData/AmericanDataAug -logger -comb_ds --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
+params = ["-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AfricanData -logger -comb_ds -p 4 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
+          "-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AfricanData -logger -comb_ds -p 3 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
+          "-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AfricanData -logger -comb_ds -p 2 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
+          "-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AfricanData -logger -comb_ds -p 1 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
+          "-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AfricanData -logger -comb_ds -p 0 --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
+          "-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AfricanData -logger -comb_ds --tar_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -ft -ft_p 4",
          ]
 
-params2 = [
+params2 = ["-epo 50 --src_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -logger -lr 1e-5",
+           "-epo 60 --src_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -logger -lr 1e-5",
+           "-epo 70 --src_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -logger -lr 1e-5",
+           "-epo 80 --src_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -logger -lr 1e-5",
+           "-epo 90 --src_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -logger -lr 1e-5",
+           "-epo 100 --src_path /home/shared_data/salmonella_detection/OriginalData/AmericanData -logger -lr 1e-5",
           ]
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
@@ -23,5 +28,5 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
 
 print("First stage tasks are done.")
 
-# with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-#     executor.map(run_task, params2)
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    executor.map(run_task, params2)
